36be1cd5c54a1652c82a0f7e1339b0dfd9d90947
Abiodun M. Abidemi <abbeycite084@gmail.com>
2021-09-23 05:32:04 -0500

    Add files via upload

---
+++ forloop_range.py
@@ -0,0 +1,11 @@
+for x in range(0, 10, 1):
+    print(x)
+print("The for loop has finished")
+
+for y in range(0, 10, 2):
+    print(y)
+print("The for loop with step 2 has finished")
+
+for z in range(-5, 10, -1):
+    print(z)
+print("The for loop has finshed")
